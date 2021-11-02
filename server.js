bd1a57f67a34d64db7ec4fe36ba7e4f9cc983243
mbakin <mehmetberkakinn@outlook.com>
2021-11-02 14:15:51 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const cors = require('cors');
+
+console.log('test `npm run dev` script')
